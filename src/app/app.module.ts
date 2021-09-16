71c139c2c4fb225db846db1a151f11f3d435cb3b
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-09-16 17:57:16 +0530

    First_Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { TestComponent } from './Test/test.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ServersComponent } from './servers/servers.component';
     AppComponent,
     ServerComponent,
     TestComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
