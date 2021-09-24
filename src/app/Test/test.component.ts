b1fa30824601c834d421956f34deed893c039319
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-09-24 15:12:30 +0530

    Assignment 2

--- src/app/Test/test.component.ts
+++ src/app/Test/test.component.ts
@@ -1,12 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 
 @Component({
     selector: 'app-test-server',
     templateUrl: './test.component.html'
+
 })
 
 
-export class TestComponent{
+export class TestComponent implements OnInit {
+
+    Username = '';
+
+
+    constructor() {
+
+    }
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    onResetUsername() {
+        this.Username = '';
+    }
 
 }
