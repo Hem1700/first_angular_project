b19f2ce23e5613c59612cceb702a64fce6613e68
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-09-28 14:48:53 +0530

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,15 +2,23 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html' 
+    templateUrl: './server.component.html'
 })
 
 export class ServerComponent {
-   serverId = 10;
-   serverStatus = 'offline'
+    serverId = 10;
+    serverStatus = 'offline';
 
-   getServerStatus(){
-       return this.serverStatus
-   }
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+
+    getColor(){
+        return this.serverStatus === 'online'? 'green' : 'red'
+    }
+    getServerStatus() {
+        return this.serverStatus
+    }
 
 }
