08b54a8762223343337bd63b1adeadf48c420b0d
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-09-30 14:46:35 +0530

    Displaying timestamp in 3rd Assignment

--- src/app/Test/test.component.ts
+++ src/app/Test/test.component.ts
@@ -28,7 +28,8 @@ export class TestComponent implements OnInit {
 
     onToggleDetails(){
       this.showSecret = !this.showSecret;
-      this.log.push(this.log.length+1);
+      //this.log.push(this.log.length+1);
+      this.log.push(new Date())
 
     }
     
